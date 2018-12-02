1eabafa8e9a12fa6aa40c70acddca1d91f9ad2a2
Ethan Pilot <ethanlpilot@gmail.com>
2018-12-02 00:55:05 -0500

    Day 2

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const day1 = require('./day1');
+const day2 = require('./day2');
 
 // eslint-disable-next-line
-console.log(`Part One: ${day1.partOne()}`);
+// console.log(`Part One: ${day1.partOne()}`);
 // eslint-disable-next-line
-console.log(`Part Two: ${day1.partTwo()}`);
+// console.log(`Part Two: ${day1.partTwo()}`);
+
+// eslint-disable-next-line
+console.log(`Part One: ${day2.partOne()}`);
+// eslint-disable-next-line
+console.log(`Part Two: ${day2.partTwo()}`);
